1f01aefab741b9fd8543b2f1b8684a60f173a86d
soul-edge <122464209+soul-edge@users.noreply.github.com>
2026-04-02 18:50:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 function toggleLogin() {
     const modal = document.getElementById('loginModal');
-    modal.classList.toggle('show');
+    if (modal.style.display === "flex") {
+        modal.style.display = "none";
+    } else {
+        modal.style.display = "flex";
+    }
 }
 
 async function handleLogin() {
     const email = document.getElementById('userEmail').value;
     const pass = document.getElementById('userPass').value;
-    if(!email || !pass) return alert("Enter details");
-
     try {
         await window.signIn(window.auth, email, pass);
         toggleLogin();
-    } catch (e) { alert(e.message); }
-}
-
-async function handleSignUp() {
-    const email = document.getElementById('userEmail').value;
-    const pass = document.getElementById('userPass').value;
-    if(pass.length < 6) return alert("Password too short");
-
-    try {
-        await window.signUp(window.auth, email, pass);
-        alert("Account Created!");
-        toggleLogin();
-    } catch (e) { alert(e.message); }
+    } catch (error) {
+        alert(error.message);
+    }
 }
