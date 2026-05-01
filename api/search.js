d61295b20c5fc079265bcea61459d6dd1052b78c
soul-edge <122464209+soul-edge@users.noreply.github.com>
2026-05-01 18:39:53 +0200

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,14 +1,18 @@
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    const { q, mangaId } = req.query;
+    const { q, mangaId, chapterId } = req.query;
 
     try {
         let targetUrl;
-        // If we have a mangaId, we are looking for chapters
-        if (mangaId) {
+        // 1. Fetching specific pages for a chapter
+        if (chapterId) {
+            targetUrl = `https://api.mangadex.org/at-home/server/${chapterId}`;
+        }
+        // 2. Fetching chapter list for a manga
+        else if (mangaId) {
             targetUrl = `https://api.mangadex.org/manga/${mangaId}/feed?translatedLanguage[]=en&order[chapter]=asc&limit=100`;
         } 
-        // Otherwise, we are searching for the Manga ID by title
+        // 3. Searching for a Manga ID by title
         else {
             targetUrl = `https://api.mangadex.org/manga?title=${encodeURIComponent(q)}&limit=1&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica`;
         }
